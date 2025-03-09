8c4e6ba18db0c6cb473585cfac6c76f781b5e1a1
manabu-nakamura <ma.nakamura@gmail.com>
2025-03-09 13:57:03 +0900

    Update

--- lib/aaa.rb
+++ lib/aaa.rb
@@ -1,15 +1,26 @@
-module Jekyll
-  module Converters
-    class Markdown
-      class KramdownParser
-        alias :old_convert :convert
-        def convert(content)
-          unless content.empty?
-            content = content.dup unless content.frozen?
-            content.gsub!(/--(\w+)--/, "<s>\\1</s>")
-          end
-          old_convert(content)
-        end
+module JekyllGFMAdmonitions2
+  class GFMAdmonitionConverter2 < Jekyll::Generator
+    def generate(site)
+      process_posts(site)
+      process_pages(site)
+    end
+
+    def process_posts(site)
+      site.posts.docs.each do |doc|
+        process_doc_content(doc)
+      end
+    end
+
+    def process_pages(site)
+      site.pages.each do |page|
+        process_doc_content(page)
+      end
+    end
+
+    def process_doc_content(doc)
+      unless doc.content.empty?
+        doc.content = doc.content.dup unless doc.content.frozen?
+        doc.content.gsub!(/--(\w+)--/, "<s>\\1</s>")
       end
     end
   end
