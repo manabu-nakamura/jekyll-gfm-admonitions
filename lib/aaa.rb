814133ba7fbe57bb3471737aa43281d76c17de1e
manabu-nakamura <ma.nakamura@gmail.com>
2025-03-09 14:14:16 +0900

    Update

--- lib/aaa.rb
+++ lib/aaa.rb
@@ -18,10 +18,6 @@ module JekyllGFMAdmonitions2
     end
 
     def process_doc_content(doc)
-      unless doc.content.empty?
-        doc.content = doc.content.dup unless doc.content.frozen?
-        doc.content.gsub!(/--(\w+)--/, "<s>\\1</s>")
-      end
     end
   end
 end
