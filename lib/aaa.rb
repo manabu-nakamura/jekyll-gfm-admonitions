8a09393482d39f6a14455774987347b473be8881
manabu-nakamura <ma.nakamura@gmail.com>
2025-03-09 19:46:21 +0900

    Update

--- lib/aaa.rb
+++ lib/aaa.rb
@@ -26,8 +26,8 @@ module JekyllGFMAdmonitions2
       unless doc.content.empty?
         doc.content = doc.content.dup unless doc.content.frozen?
 #        doc.content.gsub!(/--(\w+)--/, '<s>\1</s>')#o
-        doc.content.gsub!(/--(\w+)--/) { '<s>\1</s>' }#x\1
-#        doc.content.gsub!(/--(\w+)--/) { '<s>\\1</s>' }#x
+#        doc.content.gsub!(/--(\w+)--/) { '<s>\1</s>' }#x\1
+        doc.content.gsub!(/--(\w+)--/) { '<s>\\1</s>' }#x
       end
     end
   end
