a4f3dfa2780378df52aaf051c618797c2a60c347
manabu-nakamura <ma.nakamura@gmail.com>
2025-03-09 23:33:46 +0900

    Update

--- lib/aaa.rb
+++ lib/aaa.rb
@@ -15,7 +15,7 @@ module Jekyll
 
     def convert(content)
 #      content.upcase
-      content.gsub!(/(<blockquote>[.\s]*<\/blockquote>)/, '\1'.upcase)
+      content.gsub!(/(<blockquote>.*<\/blockquote>)/m, '\1'.upcase)
       content
     end
   end
